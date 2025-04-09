fdc006a3dee1c3209333f53a202c328f66da10bf
Nuga25 <osinugaifeoluwa1@gmail.com>
2025-04-09 13:07:43 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const accordions = document.querySelectorAll(".accordion-item");
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    accordion.classList.toggle("active");
+  });
+});
+
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav-box2");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+//Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+//thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
